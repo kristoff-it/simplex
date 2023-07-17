25d6938287b134a803be99a7b8b1de02832c2a3d
Loris Cro <kappaloris@gmail.com>
2023-07-17 19:41:48 +0200

    use an editor to edit the message for vastly improved ergonomics

--- post.sh
+++ post.sh
@@ -2,7 +2,19 @@
 
 set -eu
 
-t update "$1"
-bsky post "$1"
-toot post "$1"
+# Create a temporary file
+temp_file=$(mktemp)
 
+# Get text from default editor, or vim
+${EDITOR:-vi} "$temp_file"
+
+text=$(<"$temp_file")
+
+if [ ! -z "$text" -a "$text"!=" " ]; then
+  t update "$text"
+  bsky post "$text"
+  toot post "$text"
+fi
+
+# Clean up
+rm "$temp_file"
