1eaac6ec72b1bd2c09954689ed3e5b8c98b0196e
Loris Cro <kappaloris@gmail.com>
2023-07-19 16:39:16 +0200

    added support for images

--- post.sh
+++ post.sh
@@ -2,6 +2,13 @@
 
 set -eu
 
+# Max 4 img arguments
+
+if [ "$#" -gt 4 ]; then
+  echo "Max 4 images per message."
+  exit 1
+fi
+
 # Create a temporary file
 temp_file=$(mktemp)
 
@@ -10,13 +17,27 @@ ${EDITOR:-vi} "$temp_file"
 
 text=$(<"$temp_file")
 
-if [ ! -z "$text" ] && [ "$text" != " " ]; then
-  t update "$text"  || echo "failed to post: t"
-  toot post "$text" || echo "failed to post: toot"
-  bsky post "$text" || echo "failed to post: bsky"
-else
+if [ -z "$text" ] || [ "$text" == " " ]; then
   echo "Aborted."
+  exit 1
 fi
+
+# Twitter
+twitter_imgs=()
+for file in "$@"; do
+  twitter_imgs+=("--file" "$file")
+done
+
+t update "$text"  "${twitter_imgs[@]}" || echo "failed to post: t"
+
+# Mastodon and Bluesky
+other_imgs=()
+for file in "$@"; do
+  other_imgs+=("--image" "$file")
+done
+
+toot post "$text" "${other_imgs[@]}" || echo "failed to post: toot"
+bsky post "$text" "${other_imgs[@]}" || echo "failed to post: bsky"
 
 # Clean up
 rm "$temp_file"
